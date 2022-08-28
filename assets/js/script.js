5c3f9011b0064ac60eb3bec11f7bc715e3e25cf6
angrypin <arifinmblygf@gmail.com>
2022-08-28 15:07:14 +0700

    heading wrapper

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const activator = () => {
+	const wrappers = [
+		'#job-wrapper', '#company-wrapper'
+	];
+
+	wrappers.forEach(wrapper => {
+		const el = $(wrapper);
+		let scrollTop = $(document).scrollTop();
+		let wrapperTop = el.offset().top;
+
+		if (scrollTop >= wrapperTop) {
+			el.addClass('active');
+		} else {
+			el.removeAttr('class');
+		}
+	});
+}
+
+$(window).scroll(activator);
