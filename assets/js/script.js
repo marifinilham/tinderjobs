02b0f9f6ac72af132ce52405b081c7b10d76197f
angrypin <arifinmblygf@gmail.com>
2022-08-28 20:14:21 +0700

    theme toggler

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,4 +16,23 @@ const activator = () => {
 	});
 }
 
+const theme = (val) => {
+	localStorage.theme = val !== undefined ? val : 'dark';
+	
+	let dark = localStorage.theme === 'dark';
+	
+	if (dark) {
+		$('head').append('<link rel="stylesheet" type="text/css" href="assets/css/dark.css">');
+	} else {
+		$('link[href*="dark.css"]').remove();
+	}
+}
+
+$('.mode input').click(function() {
+	theme(this.checked ? 'dark' : 'light');
+});
+
+if (localStorage.theme === 'dark')
+	$('.mode input').click();
+
 $(window).scroll(activator);
